1beb56fe2e838e4fb5e929c2b82e725b6f1a555e
LaterNerds <theo.ls.raymond@icloud.com>
2024-05-11 07:39:30 -0400

    added map

--- score.gd
+++ score.gd
@@ -5,6 +5,7 @@ var score = 0
 func _process(delta):
 	score += delta
 	set_text(str(round_to_dec(score, 3) * 1000))
+	position = Vector2(get_node("../player").position.x - 550, get_node("../player").position.y - 300)
 
 func round_to_dec(num, digit):
 	return round(num * pow(10.0, digit)) / pow(10.0, digit)
